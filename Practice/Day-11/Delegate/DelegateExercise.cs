072c44cdb2d489b30846ac7dfe536c935219e003
muskan.2 <muskan.2@DEL1-LHP-N82179>
2022-08-16 21:35:05 +0530

    added

---
+++ Practice/Day-11/Delegate/DelegateExercise.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class DelegateExercise
+    {
+        public delegate void MyDelagate(int i);
+        public void Method1(int i)
+        {
+            Console.WriteLine("This is Method 1 th i is: "+i);
+        }
+        public void Method3()
+        {
+            Console.WriteLine("This is Method 3");
+        }
+        public void Method2()
+        {
+           
+            MyDelagate mydelObj = new MyDelagate(Method1);
+            mydelObj(50);
+
+        }
+    }
+}
